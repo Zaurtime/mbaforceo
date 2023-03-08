6a6f25dd4ad6638187d2e04cd0a712364f6e35c1
Zaurtime <zaurteam@gmail.com>
2023-03-08 13:18:11 +0000

    Add url to logout account in base html

--- mbaforceo/urls.py
+++ mbaforceo/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("mba.urls"), name="mba-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("allauth.urls")),
 ]
