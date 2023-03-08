8a25ead06a1904ee32ec41df2c05f400beb1bd42
Zaurtime <zaurteam@gmail.com>
2023-03-08 13:06:09 +0000

    Correct allauth setting accaount

--- mbaforceo/settings.py
+++ mbaforceo/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'allauth',
-    'allauth.accaunt',
+    'allauth.account',
     'allauth.socialaccaunt',
     'cloudinary_storage',
     'django.contrib.staticfiles',
