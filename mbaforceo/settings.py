48b5d7b18b303257696db0c894afa8f782a43e00
Zaurtime <zaurteam@gmail.com>
2023-03-08 12:18:22 +0000

    Add install apps allauth

--- mbaforceo/settings.py
+++ mbaforceo/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'allauth',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
