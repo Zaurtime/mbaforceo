274882577d79a6d986dc65002c71516fde4ea0f5
Zaurtime <zaurteam@gmail.com>
2023-03-09 10:44:02 +0000

    Add post comments function

--- mba/views.py
+++ mba/views.py
@@ -13,7 +13,8 @@ class PostList(generic.ListView):
 
 class PostDetail(View):
     
-    def get(self, request, slug, *args, **kwargs):
+    
+        )def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by("-created_on")
@@ -30,4 +31,22 @@ class PostDetail(View):
                 "liked": liked,
                 "comment_form": CommentForm()
             },
-        )
+
+def post(self, request, slug, *args, **kwargs):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.filter(approved=True).order_by("-created_on")
+    liked = False
+    if post.likes.filter(id=self.request.user.id).exists():
+        liked = True
+    
+    
+    return render(
+        request,
+        "post_detail.html",
+         {
+            "post": post,
+            "comments": comments,
+            "liked": liked,
+            "comment_form": CommentForm()
+            },
