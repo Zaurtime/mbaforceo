1363a6595ecee31bff0b0c2d513e756ec39a69a8
Zaurtime <zaurteam@gmail.com>
2023-03-22 12:17:48 +0000

    Create new class for signup form and import user creation form

--- mba/forms.py
+++ mba/forms.py
@@ -1,6 +1,8 @@
 from .models import Comment
 from django import forms 
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class CommentForm(forms.ModelForm):
@@ -24,4 +26,10 @@ class UpdateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['featured_image', 'title', 'content', ]
+
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'pasword2',]
       
